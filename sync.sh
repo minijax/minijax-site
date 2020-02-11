10be66153748b44be8dc2ac77ee4a80e4270ca81
Cody Ebberson <cody@ebberson.com>
2020-02-10 23:58:14 -0800

    Remove old sync file

--- sync.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Copy files to S3
-aws s3 cp --recursive ./public s3://minijax.org/ --region us-west-2 --exclude ".DS_Store"
-
-# Create CloudFront invalidation
-aws cloudfront create-invalidation --distribution-id EB74ISGX86TTQ --paths "/index.html"
-
